0772cb7c4118f540787785a4ae708c86f37467aa
szyda <szydasylwia@protonmail.com>
2023-12-16 21:58:14 +0100

    fences passed tests

--- heap.c
+++ heap.c
@@ -1,12 +1,13 @@
-#include <stdint.h>
-#include <stdio.h>
 #include <stddef.h>
 #include <string.h>
 #include "heap.h"
 
 #define METADATA sizeof(struct memory_chunk_t)
-#define FENCE 'x'
 #define FENCE_SIZE 16
+
+char fence_before[] = "xxxxxxxxxxxxxxx";
+char fence_after[] = "yyyyyyyyyyyyyyy";
+
 
 void memory_init(void *address, size_t size) {
     if (address != NULL && size > 0) {
@@ -16,16 +17,18 @@ void memory_init(void *address, size_t size) {
     }
 }
 
-void *memory_malloc(size_t size) {
-    if (size == 0) return NULL;
+void *memory_malloc(size_t size){
+    if (size <= 0) return NULL;
     if (heap_validate()) return NULL;
+
+    char* data;
 
     // case when memory is empty
     struct memory_chunk_t *current_block = memory_manager.first_memory_chunk;
     if (current_block == NULL) {
         if (size + METADATA > memory_manager.memory_size) return NULL;
 
-        current_block = (struct memory_chunk_t*) memory_manager.memory_start;
+        current_block = (struct memory_chunk_t *) memory_manager.memory_start;
         memory_manager.first_memory_chunk = current_block;
 
         current_block->prev = NULL;
@@ -33,109 +36,118 @@ void *memory_malloc(size_t size) {
         current_block->size = size;
         current_block->free = 0;
 
-        memset(((uint8_t *)current_block + METADATA), FENCE, FENCE_SIZE);
-        memset(((uint8_t *)current_block + METADATA + size + FENCE_SIZE), FENCE, FENCE_SIZE);
+        data = (char *)current_block + METADATA;
+        memcpy(data, fence_before, FENCE_SIZE);
 
-        return (void *) ((uint8_t *)current_block + METADATA + FENCE_SIZE);
+        data = (char *)current_block + METADATA + FENCE_SIZE + current_block->size;
+        memcpy(data, fence_after, FENCE_SIZE);
+
+        return (void *) ((char *)current_block + METADATA + FENCE_SIZE);
     }
 
-    size_t count_size = 0;
-    struct memory_chunk_t *last_memory_block = NULL;
+    size_t counted_size = 0;
     current_block = memory_manager.memory_start;
 
-    // case when memory is not empty
     while (current_block) {
-        if (current_block->free == 1 && current_block->size >= size + METADATA + 2 * FENCE_SIZE) {
+        counted_size += METADATA + current_block->size + 2 * FENCE_SIZE;
+
+        if (current_block->free == 1 && current_block->size >= size + 2 * FENCE_SIZE) {
             current_block->size = size;
             current_block->free = 0;
 
-            memset((uint8_t *)current_block + METADATA, FENCE, FENCE_SIZE);
-            memset((uint8_t *)current_block + METADATA + size + FENCE_SIZE, FENCE, FENCE_SIZE);
+            data = (char *)current_block + METADATA;
+            memcpy(data, fence_before, FENCE_SIZE);
 
-            return (void *) ((uint8_t *)current_block + METADATA + FENCE_SIZE);
+            data = (char *)current_block + METADATA + FENCE_SIZE + current_block->size;
+            memcpy(data, fence_after, FENCE_SIZE);
+
+            return (void *) ((char *) current_block + METADATA + FENCE_SIZE);
         }
 
-        count_size += current_block->size + METADATA + 2 * FENCE_SIZE;
-        last_memory_block = current_block;
+        if (current_block->next == NULL) {
+            if (counted_size + size + METADATA + 2 * FENCE_SIZE <= memory_manager.memory_size) {
+                struct memory_chunk_t* prev_block = current_block;
+                current_block = (struct memory_chunk_t*)((char*)prev_block + METADATA + current_block->size + 2 * FENCE_SIZE);
+
+                prev_block->next = current_block;
+                current_block->size = size;
+                current_block->next = NULL;
+                current_block->prev = prev_block;
+                current_block->free = 0;
+
+                data = (char *)current_block + METADATA;
+                memcpy(data, fence_before, FENCE_SIZE);
+
+                data = (char *)current_block + METADATA + FENCE_SIZE + current_block->size;
+                memcpy(data, fence_after, FENCE_SIZE);
+
+                return (void *) ((char *) current_block + METADATA + FENCE_SIZE);
+            } else {
+                return NULL;
+            }
+        }
+
         current_block = current_block->next;
-    }
-
-    // if we didn't find any block
-    // we can create block at the end of list if there is enough memory
-    if (count_size + METADATA + size + 2 * FENCE_SIZE <= memory_manager.memory_size) {
-        current_block = (struct memory_chunk_t *)((uint8_t *) last_memory_block + last_memory_block->size + FENCE_SIZE);
-        current_block->prev = last_memory_block;
-        last_memory_block->next = current_block;
-
-        current_block->next = NULL;
-        current_block->size = size;
-        current_block->free = 0;
-
-        memset((uint8_t *)current_block + METADATA, FENCE, FENCE_SIZE);
-        memset((uint8_t *)current_block + METADATA + size + FENCE_SIZE, FENCE, FENCE_SIZE);
-
-        return (void *)((uint8_t *)current_block + METADATA + FENCE_SIZE);
     }
 
     return NULL;
 }
 
-void memory_free(void *address) {
+void memory_free(void *address){
     if (address == NULL || memory_manager.first_memory_chunk == NULL) return;
-
-    struct memory_chunk_t *current_metadata = (struct memory_chunk_t *)((uint8_t *)address - METADATA - FENCE_SIZE);
-
-    if (((char *)address - METADATA - FENCE_SIZE) < (char *)memory_manager.memory_start) return;
-
-    if (validate_address(current_metadata)) return;
     if (heap_validate()) return;
 
-    if (current_metadata->free != 0 && current_metadata->free != 1) return;
-    if (current_metadata->size > memory_manager.memory_size) return;
+    struct memory_chunk_t * current_block = (struct memory_chunk_t*)((char*)address - METADATA - FENCE_SIZE);
 
-    if (current_metadata->free == 0) {
-        current_metadata->free = 1;
+    if (validate_address(current_block)) return;
+    if (current_block->free != 0 && current_block->free != 1) return;
+    if (current_block->size > memory_manager.memory_size) return;
 
-        if (current_metadata->next == NULL && current_metadata->prev == NULL) {
+    if (current_block->free == 0){
+        current_block->free = 1;
+
+        if (current_block->next == NULL && current_block->prev == NULL){
             memory_manager.first_memory_chunk = NULL;
             return;
         }
 
-        if (current_metadata->next != NULL) {
-            current_metadata->size = (char *)current_metadata->next - (char *)current_metadata + FENCE_SIZE;
+        if (current_block->next != NULL){
+            char * start = (char*)current_block + METADATA;
+            char * end = (char*)current_block->next;
+
+            current_block->size = end - start;
         }
 
-        // if next not null
-        if (current_metadata->next != NULL && current_metadata->next->free == 1) {
-            struct memory_chunk_t *next_block = current_metadata->next;
-            current_metadata->next = next_block->next;
+        if (current_block->prev != NULL) {
+            if (current_block->prev->free == 1) {
+                struct memory_chunk_t *prev_block = current_block->prev;
+                prev_block->next = current_block->next;
+                prev_block->size = METADATA + prev_block->size + current_block->size;
 
-            if (next_block->next != NULL) {
-                next_block->next->prev = current_metadata;
-            }
+                if (current_block->next != NULL){
+                    current_block->next->prev = prev_block;
+                }
 
-            current_metadata->size = current_metadata->size + next_block->size + METADATA + 2 * FENCE_SIZE;
-        }
+                current_block = prev_block;
 
-        // if prev not null
-        if (current_metadata->prev != NULL && current_metadata->prev->free == 1) {
-            struct memory_chunk_t *prev_block = current_metadata->prev;
-
-            prev_block->next = current_metadata->next;
-            prev_block->size = prev_block->size + current_metadata->size;
-
-            if (current_metadata->next != NULL) {
-                current_metadata->next->prev = prev_block;
-            }
-
-            current_metadata = prev_block;
-
-            if (current_metadata->next == NULL && current_metadata->prev != NULL) {
-                current_metadata->prev->next = NULL;
+                if (current_block->next == NULL && current_block->prev != NULL) {
+                    current_block->prev->next = NULL;
+                }
             }
         }
 
-        if (current_metadata->next == NULL && current_metadata->prev == NULL) {
+        if (current_block->next != NULL && current_block->next->free == 1) {
+            struct memory_chunk_t *next_block = current_block->next;
+            current_block->next = next_block->next;
+
+            if (next_block->next != NULL){
+                next_block->next->prev = current_block;
+            }
+
+            current_block->size = METADATA + next_block->size + current_block->size;
+        }
+
+        if (current_block->next == NULL && current_block->prev == NULL){
             memory_manager.first_memory_chunk = NULL;
             return;
         }
@@ -151,13 +163,13 @@ int heap_validate(void) {
             // before data
             char *data = (char *) memory_block + METADATA;
 
-            if (strncmp("xxxxxxxxxxxxxxxx", data, FENCE_SIZE) !=0) {
+            if (strncmp(fence_before, data, FENCE_SIZE) !=0) {
                 return 1;
             }
 
             // after data
             data = (char *) memory_block + METADATA + FENCE_SIZE + memory_block->size;
-            if (strncmp("xxxxxxxxxxxxxxxx", data, FENCE_SIZE) != 0) {
+            if (strncmp(fence_after, data, FENCE_SIZE) != 0) {
                 return 1;
             }
         }
